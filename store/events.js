6d68ced67ea1f771d13a7d62cbae50b50753db05
Tom Myruski <thomas.myruski@gmail.com>
2022-05-17 15:17:40 -0400

    Testing remote server

--- store/events.js
+++ store/events.js
@@ -20,7 +20,8 @@ export const useEventStore = defineStore('eventStore', {
         // if events is already set, stop
         if (this.events.length) return;
         try {
-            let events = await fetch(`http://jamstack.local/wp-json/wp/v2/events?page=1&per_page=100&_embed=1`).then(res => res.json());
+            // let events = await fetch(`http://jamstack.local/wp-json/wp/v2/events?page=1&per_page=100&_embed=1`).then(res => res.json());
+            let events = await fetch(`http://jamstack.flywheelsites.com/wp-json/wp/v2/events?page=1&per_page=100&_embed=1`).then(res => res.json());
             // filter out unnecessary data
             events = events.filter(el => el.status === "publish").map(({ id, slug, title, content, acf }) => ({
                 id,
